f3bc6dc6851557f97d8d115570d11b5ca5f38789
Piotr Florczak <piotrflorczakk@gmail.com>
2021-08-04 15:27:24 +0200

    Dodanie poprawnej walidacji hasła

--- bitad-frontend/src/components/RegistrationForm/FormTextInput.js
+++ bitad-frontend/src/components/RegistrationForm/FormTextInput.js
@@ -4,12 +4,17 @@ export function FormTextInput({ register, name, errors, ...rest }) {
   const maxLength = { value: 50, message: "Maksymalnie 50 znaków" };
   const minLength = { value: 3, message: "Minimum 3 znaki" };
   const required = "Pole wymagane";
+  const { onChange, ...a } = register(name, { required, minLength, maxLength });
   return (
     <div className={styles.section__field}>
       <label htmlFor={name}>{rest.labelText}</label>
       <input
         id={name}
-        {...register(name, { required, minLength, maxLength })}
+        onChange={(e) => {
+          if (typeof rest.onChange === "function") rest?.onChange();
+          onChange(e);
+        }}
+        {...a}
         type={rest.type ? rest.type : "text"}
         className={styles.field__input}
       />
