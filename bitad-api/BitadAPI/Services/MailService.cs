19a7fe8faa2271253723c056584d71be669212a1
rtrydev <r.kulka44@gmail.com>
2021-10-23 16:47:50 +0200

    stylowanie maila z linkiem do resetu hasla

--- bitad-api/BitadAPI/Services/MailService.cs
+++ bitad-api/BitadAPI/Services/MailService.cs
@@ -77,8 +77,26 @@ namespace BitadAPI.Services
             message.From.Add(new MailboxAddress("Bitad2021", _emailAddress));
             message.To.Add(new MailboxAddress(receiver, address));
             message.Subject = "Resetowanie hasła konta Bitad2021";
-            var text = $"{_serverUrl}/password-reset/{resetCode}";
-            message.Body = new TextPart("plain"){ Text = text };
+            string FullFormatPath = "/app/bitad-email-template";
+
+            string HtmlFormat = string.Empty;
+
+            var builder = new BodyBuilder();
+
+            using (FileStream fs = new FileStream(Path.Combine(FullFormatPath, "index_password.html"), FileMode.Open))
+            {
+                using (StreamReader sr = new StreamReader(fs))
+                {
+                    HtmlFormat = sr.ReadToEnd();
+                }
+            }
+
+            HtmlFormat = HtmlFormat.Replace("RESET_LINK", $"{_serverUrl}/password-reset/{resetCode}");
+            HtmlFormat = HtmlFormat.Replace("USER_NAME", receiver);
+
+            builder.HtmlBody = HtmlFormat;
+
+            message.Body = builder.ToMessageBody();
             
             using (var client = new SmtpClient()) {
                 await client.ConnectAsync (_smtpServer, 587, SecureSocketOptions.StartTls);
