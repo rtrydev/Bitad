2eac8b70b6b6e13d6ea502624be0c5ab3bab31ad
rtrydev <r.kulka44@gmail.com>
2021-10-25 15:13:46 +0200

    migration

---
+++ bitad-api/BitadAPI/Migrations/20211025131323_userBan.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BitadAPI.Migrations
+{
+    public partial class userBan : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "banned_from_roulette",
+                table: "users",
+                type: "bool",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<DateTime>(
+                name: "workshop_attendance_check_date",
+                table: "users",
+                type: "timestamp",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "banned_from_roulette",
+                table: "users");
+
+            migrationBuilder.DropColumn(
+                name: "workshop_attendance_check_date",
+                table: "users");
+        }
+    }
+}
