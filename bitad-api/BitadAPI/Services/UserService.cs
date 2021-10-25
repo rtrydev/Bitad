3011251d6f3402b05d86425812eea7e08ee8bc3b
rtrydev <r.kulka44@gmail.com>
2021-10-25 19:28:05 +0200

    check attendance on workshop

--- bitad-api/BitadAPI/Services/UserService.cs
+++ bitad-api/BitadAPI/Services/UserService.cs
@@ -28,6 +28,8 @@ namespace BitadAPI.Services
         public Task<DtoUser> ResetPassword(string resetCode, string newPassword);
         public Task<DtoUser> ResendActivation(string username);
         public Task<DtoUser> ConfirmAccount(string confirmCode);
+        public Task<TokenRefreshResponse<DtoAttendanceResult>> CheckAttendanceWorkshop(int issuerId,
+            string attendanceCode, string workshopCode);
     }
 
     public class UserService : IUserService
@@ -281,6 +283,77 @@ namespace BitadAPI.Services
             }
             
             user.AttendanceCheckDate = DateTime.Now;
+            var result = await _userRepository.UpdateUser(user);
+            return new TokenRefreshResponse<DtoAttendanceResult>
+            {
+                Body = new DtoAttendanceResult {Code = 0, Message = "Ok"},
+                Token = refreshToken,
+                Code = 0
+            }; 
+
+        }
+        
+        public async Task<TokenRefreshResponse<DtoAttendanceResult>> CheckAttendanceWorkshop(int issuerId, string attendanceCode, string workshopCode)
+        {
+            var issuer = await _userRepository.GetById(issuerId);
+            var refreshToken = await _jwtService.GetNewToken(issuerId);
+            if (issuer.Role == UserRole.Guest)
+            {
+                return new TokenRefreshResponse<DtoAttendanceResult>
+                {
+                    Body = new DtoAttendanceResult {Code = 401, Message = "Unauthorized"},
+                    Token = refreshToken,
+                    Code = 403
+                };
+            }
+                
+            var user = await _userRepository.GetByPredicate(x => x.WorkshopAttendanceCode == attendanceCode);
+            if (user is null)
+            {
+                user = await _userRepository.GetByPredicate(x => x.Email == attendanceCode);
+            }
+            
+            if (user is null)
+            {
+                return new TokenRefreshResponse<DtoAttendanceResult>
+                {
+                    Body = new DtoAttendanceResult {Code = 404, Message = "No such user"},
+                    Token = refreshToken,
+                    Code = 404
+                };
+            }
+
+            if (user.Workshop.Code != workshopCode)
+            {
+                return new TokenRefreshResponse<DtoAttendanceResult>
+                {
+                    Body = new DtoAttendanceResult {Code = 404, Message = "No such user"},
+                    Token = refreshToken,
+                    Code = 404
+                };
+            }
+
+            if (user.ActivationDate is null)
+            {
+                return new TokenRefreshResponse<DtoAttendanceResult>
+                {
+                    Body = new DtoAttendanceResult {Code = 2, Message = "Not activated"},
+                    Token = refreshToken,
+                    Code = 2
+                };
+            }
+            
+            if (user.WorkshopAttendanceCheckDate is not null)
+            {
+                return new TokenRefreshResponse<DtoAttendanceResult>
+                {
+                    Body = new DtoAttendanceResult {Code = 1, Message = "Already checked"},
+                    Token = refreshToken,
+                    Code = 1
+                };
+            }
+            
+            user.WorkshopAttendanceCheckDate = DateTime.Now;
             var result = await _userRepository.UpdateUser(user);
             return new TokenRefreshResponse<DtoAttendanceResult>
             {
