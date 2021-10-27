522c2ecde15aab2eb9b039aa5891638094876ce1
rtrydev <r.kulka44@gmail.com>
2021-10-27 13:59:24 +0200

    migration

---
+++ bitad-api/BitadAPI/Migrations/20211027115908_infoLink.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BitadAPI.Migrations
+{
+    public partial class infoLink : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "external_link",
+                table: "workshops",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "short_info",
+                table: "workshops",
+                type: "text",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "external_link",
+                table: "workshops");
+
+            migrationBuilder.DropColumn(
+                name: "short_info",
+                table: "workshops");
+        }
+    }
+}
