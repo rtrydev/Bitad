048d7e7a4cf62bbe856e3057920a49106f9d11ec
rtrydev <r.kulka44@gmail.com>
2021-10-28 10:39:12 +0200

    migration

---
+++ bitad-api/BitadAPI/Migrations/20211028083829_removeUsername.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace BitadAPI.Migrations
+{
+    public partial class removeUsername : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "username",
+                table: "users");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "username",
+                table: "users",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
