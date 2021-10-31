223e3e2d2ec0ea8194168cd4a63e71057f91937d
rtrydev <r.kulka44@gmail.com>
2021-10-31 15:49:57 +0100

    fix authorization

--- bitad-api/BitadAPI/Controllers/BitadController.cs
+++ bitad-api/BitadAPI/Controllers/BitadController.cs
@@ -11,37 +11,17 @@ namespace BitadAPI.Controllers
     {
         protected async Task<ActionResult<TResult>> MakeAuthorizedServiceCall<TResult>(Func<int, Task<TokenRefreshResponse<TResult>>> serviceCall, IJwtService jwtService)
         {
-            var authorizationResult = await GetAuthorizationResult(jwtService);
-            if (authorizationResult.GetType() == typeof(UnauthorizedResult)) return Unauthorized();
-            var result = await serviceCall(authorizationResult.Value);
+            var id = Int32.Parse(User.Claims.First(p => p.Type == "id").Value);
+            var presentedToken = HttpContext.Request.Headers.FirstOrDefault(x => x.Key == "Authorization").Value;
+            if (await jwtService.CheckAuthorization(id, presentedToken) is UnauthorizedResult)
+            {
+                return Unauthorized();
+            }
+            var result = await serviceCall(id);
             return FormResult(result);
         }
         
         protected async Task<ActionResult<TResult>> MakeAuthorizedServiceCall<T, TResult>(T arg1, Func<int, T, Task<TokenRefreshResponse<TResult>>> serviceCall, IJwtService jwtService)
-        {
-            var authorizationResult = await GetAuthorizationResult(jwtService);
-            if (authorizationResult.GetType() == typeof(UnauthorizedResult)) return Unauthorized();
-            var result = await serviceCall(authorizationResult.Value, arg1);
-            return FormResult(result);
-        }
-        
-        protected async Task<ActionResult<TResult>> MakeAuthorizedServiceCall<T1, T2, TResult>(T1 arg1, T2 arg2, Func<int, T1, T2, Task<TokenRefreshResponse<TResult>>> serviceCall, IJwtService jwtService)
-        {
-            var authorizationResult = await GetAuthorizationResult(jwtService);
-            if (authorizationResult.GetType() == typeof(UnauthorizedResult)) return Unauthorized();
-            var result = await serviceCall(authorizationResult.Value, arg1, arg2);
-            return FormResult(result);
-        }
-        
-        protected async Task<ActionResult> MakeAuthorizedServiceCall(Func<int, Task<TokenRefreshResponse>> serviceCall, IJwtService jwtService)
-        {
-            var authorizationResult = await GetAuthorizationResult(jwtService);
-            if (authorizationResult.GetType() == typeof(UnauthorizedResult)) return Unauthorized();
-            var result = await serviceCall(authorizationResult.Value);
-            return FormResult(result);
-        }
-
-        private async Task<ActionResult<int>> GetAuthorizationResult(IJwtService jwtService)
         {
             var id = Int32.Parse(User.Claims.First(p => p.Type == "id").Value);
             var presentedToken = HttpContext.Request.Headers.FirstOrDefault(x => x.Key == "Authorization").Value;
@@ -49,8 +29,32 @@ namespace BitadAPI.Controllers
             {
                 return Unauthorized();
             }
-
-            return Ok(id);
+            var result = await serviceCall(id, arg1);
+            return FormResult(result);
+        }
+        
+        protected async Task<ActionResult<TResult>> MakeAuthorizedServiceCall<T1, T2, TResult>(T1 arg1, T2 arg2, Func<int, T1, T2, Task<TokenRefreshResponse<TResult>>> serviceCall, IJwtService jwtService)
+        {
+            var id = Int32.Parse(User.Claims.First(p => p.Type == "id").Value);
+            var presentedToken = HttpContext.Request.Headers.FirstOrDefault(x => x.Key == "Authorization").Value;
+            if (await jwtService.CheckAuthorization(id, presentedToken) is UnauthorizedResult)
+            {
+                return Unauthorized();
+            }
+            var result = await serviceCall(id, arg1, arg2);
+            return FormResult(result);
+        }
+        
+        protected async Task<ActionResult> MakeAuthorizedServiceCall(Func<int, Task<TokenRefreshResponse>> serviceCall, IJwtService jwtService)
+        {
+            var id = Int32.Parse(User.Claims.First(p => p.Type == "id").Value);
+            var presentedToken = HttpContext.Request.Headers.FirstOrDefault(x => x.Key == "Authorization").Value;
+            if (await jwtService.CheckAuthorization(id, presentedToken) is UnauthorizedResult)
+            {
+                return Unauthorized();
+            }
+            var result = await serviceCall(id);
+            return FormResult(result);
         }
 
         private ActionResult<T> FormResult<T>(TokenRefreshResponse<T> result)
