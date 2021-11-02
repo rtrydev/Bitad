6eab713da81a4c7f2a5bc0cad4559cc7bf6a240b
Piotr Florczak <piotr.florczak@ageno.pl>
2021-11-02 20:08:47 +0100

    Dodanie komentarzu

--- convertToWebp.sh
+++ convertToWebp.sh
@@ -4,3 +4,7 @@ do
   filesize=$(ls -la $file | awk '{print  $5}')
     cwebp -q 60 $file -o $file.webp
 done
+
+# Install 
+# on CentOs sudo yum install libwebp-tools
+# or on Linux sudo apt-get install webp
