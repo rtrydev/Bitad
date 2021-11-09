41f808289897459f6d8b00194a53047c350acca6
Piotr Florczak <piotr.florczak@ageno.pl>
2021-11-09 20:38:02 +0100

    Dostosowanie do crona

--- backup.sh
+++ backup.sh
@@ -2,5 +2,5 @@
 name=bitad.sql
 currentTime=$(date "+%Y-%m-%d_%H-%M-%S")
 fileName=$name.$currentTime
-docker-compose exec -u postgres db bash -c "cd /var/lib/postgresql && pg_dump Bitad > ${fileName}" && \
- docker cp db:/var/lib/postgresql/${fileName} ~/
+/usr/local/bin/docker-compose exec -T -u postgres db bash -c "cd /var/lib/postgresql && pg_dump Bitad > ${fileName}" && \
+ docker cp db:/var/lib/postgresql/${fileName} .
