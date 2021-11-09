eac1c30177e4e7fea216ec78f4d5a6ca82c511db
Piotr Florczak <piotr.florczak@ageno.pl>
2021-11-09 20:50:18 +0100

    Dodanie parametru

--- backup.sh
+++ backup.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
+location=$1
 name=bitad.sql
 currentTime=$(date "+%Y-%m-%d_%H-%M-%S")
 fileName=$name.$currentTime
+
+cd $location
 /usr/local/bin/docker-compose exec -T -u postgres db bash -c "cd /var/lib/postgresql && pg_dump Bitad > ${fileName}" && \
- docker cp db:/var/lib/postgresql/${fileName} .
+ docker cp db:/var/lib/postgresql/${fileName} $location
