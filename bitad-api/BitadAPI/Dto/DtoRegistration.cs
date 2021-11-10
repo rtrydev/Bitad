683ec9c406be58e5838853d4031cccc08ce3fc04
rtrydev <r.kulka44@gmail.com>
2021-11-10 15:04:58 +0100

    fix email validation

--- bitad-api/BitadAPI/Dto/DtoRegistration.cs
+++ bitad-api/BitadAPI/Dto/DtoRegistration.cs
@@ -7,7 +7,7 @@ namespace BitadAPI.Dto
 {
     public class DtoRegistration
     {
-        [RegularExpression(@"^[a-zA-Z0-9\.-_]{1,}@[a-zA-Z0-9]{1,}\.[a-zA-Z\.]{1,}$")]
+        [RegularExpression(@"^[a-zA-Z0-9\.\-_]{1,}@[a-zA-Z0-9\-_]{1,}\.[a-zA-Z\.]{1,}$")]
         [DefaultValue("a@a.com")]
         public string Email { get; set; }
         [MaxLength(24)]
